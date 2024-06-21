1c20ad2b5e904b5f20062a2a06f8354dbff48a24
phamdat2004 <phamtiendat21072004@gmai.com>
2024-06-21 12:09:07 +0700

    upload

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Formthongtin.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DangNhap() { return View(); }
+    }
+}
